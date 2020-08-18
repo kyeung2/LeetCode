0dc1f7aebbef32a96780c0b645a192a1ff51e931
Kye Yeung <yeung.kye@pm.me>
2020-08-18 12:51:36 +0100

    more random stuff

--- src/main/java/io/nimbus/leetcode/arrays/introduction/SquaresOfASortedArray.java
+++ src/main/java/io/nimbus/leetcode/arrays/introduction/SquaresOfASortedArray.java
@@ -20,8 +20,13 @@ public class SquaresOfASortedArray {
         return res;
     }
 
+    // I get the impression the website doesn't want me to implement it this way...
+    public int[] sortedSquares_stream(int[] A) {
+        return Arrays.stream(A).map(i -> i * i).sorted().toArray();
+    }
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(new SquaresOfASortedArray().sortedSquares(new int[]{-7, -3, 2, 3, 11})));
+        System.out.println(Arrays.toString(new SquaresOfASortedArray().sortedSquares_stream(new int[]{-7, -3, 2, 3, 11})));
     }
 }
