9a28149971bcd3a2ee7c5d87bc86d1b37a13a2a0
Kye Yeung <yeung.kye@pm.me>
2020-08-19 17:00:31 +0100

    arrays work

--- src/main/java/io/nimbus/leetcode/arrays/inplaceoperations/MoveZeros.java
+++ src/main/java/io/nimbus/leetcode/arrays/inplaceoperations/MoveZeros.java
@@ -1,13 +1,33 @@
 package io.nimbus.leetcode.arrays.inplaceoperations;
 
-import io.nimbus.leetcode.arrays.delete.RemoveDuplicates;
+import java.util.Arrays;
 
 /**
  * https://leetcode.com/explore/learn/card/fun-with-arrays/511/in-place-operations/3157/
  */
 public class MoveZeros {
 
+    // this one felt good. After the explanation of the  two-pointer technique I feel more confidence to apply it.
     public void moveZeroes(int[] nums) {
 
+        int writePointer = 0;
+        for (int readPointer = 0; readPointer < nums.length; readPointer++) {
+
+            if (nums[readPointer] != 0) {
+                nums[writePointer] = nums[readPointer];
+                writePointer++;
+            }
+        }
+
+        for (int zeroPointer = writePointer; zeroPointer < nums.length; zeroPointer++) {
+            nums[zeroPointer] = 0;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        new MoveZeros().moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
     }
 }
