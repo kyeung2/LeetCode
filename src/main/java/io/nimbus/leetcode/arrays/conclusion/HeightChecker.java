3159f2957ebe3c5c7719fbf0c66f466e5cbe4946
Kye Yeung <yeung.kye@pm.me>
2020-08-19 20:24:28 +0100

    arrays work

---
+++ src/main/java/io/nimbus/leetcode/arrays/conclusion/HeightChecker.java
@@ -0,0 +1,27 @@
+package io.nimbus.leetcode.arrays.conclusion;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/explore/learn/card/fun-with-arrays/523/conclusion/3228/
+ */
+public class HeightChecker {
+
+    public int heightChecker(int[] heights) {
+
+        //interesting that the Arrays.stream() version is slower, something to keep in mind
+//        int[] sortedHeights = Arrays.stream(heights).sorted().toArray();
+        int[] sortedHeights = new int[heights.length];
+        System.arraycopy(heights, 0, sortedHeights, 0, heights.length);
+        Arrays.sort(sortedHeights);
+
+        int differences = 0;
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != sortedHeights[i]) {
+                differences++;
+            }
+        }
+
+        return differences;
+    }
+}
