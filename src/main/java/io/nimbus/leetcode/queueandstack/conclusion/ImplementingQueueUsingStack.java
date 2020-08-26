490ac4a53bc43d55b0df1c7dc1a6dda91c5e979c
Kye Yeung <yeung.kye@pm.me>
2020-08-26 11:18:25 +0100

    stack work

---
+++ src/main/java/io/nimbus/leetcode/queueandstack/conclusion/ImplementingQueueUsingStack.java
@@ -0,0 +1,56 @@
+package io.nimbus.leetcode.queueandstack.conclusion;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * https://leetcode.com/explore/learn/card/queue-stack/239/conclusion/1386/
+ */
+public class ImplementingQueueUsingStack {
+    static class MyQueue {
+
+        private final Deque<Integer> stack;
+
+        public MyQueue() {
+            stack = new ArrayDeque<>();
+        }
+
+        public void push(int x) {
+            stack.push(x);
+        }
+
+        public int pop() {
+
+            Deque<Integer> temp = new ArrayDeque<>();
+            while (!stack.isEmpty())
+                temp.push(stack.pop());
+
+            int topValue = temp.pop();
+
+            while (!temp.isEmpty())
+                stack.push(temp.pop());
+
+            return topValue;
+        }
+
+        public int peek() {
+            return stack.peekLast();
+        }
+
+        public boolean empty() {
+            return stack.isEmpty();
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        MyQueue queue = new MyQueue();
+
+        queue.push(1);
+        queue.push(2);
+        System.out.println( queue.peek());  // returns 1
+        System.out.println(   queue.pop());   // returns 1
+        queue.empty(); // returns false
+    }
+}
---
+++ src/main/java/io/nimbus/leetcode/queueandstack/conclusion/ImplementingStackUsingQueue.java
@@ -0,0 +1,47 @@
+package io.nimbus.leetcode.queueandstack.conclusion;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * https://leetcode.com/explore/learn/card/queue-stack/239/conclusion/1387/
+ */
+public class ImplementingStackUsingQueue {
+    static class MyStack {
+
+        private final Queue<Integer> queue;
+
+        public MyStack() {
+            queue = new LinkedList<>();
+        }
+
+
+        // reversing the input makes the in the push() implementation makes the rest of the method implementations
+        // much simpler. Probably can refactor the ImplementingQueueUsingStack to use the same idea.
+        public void push(int x) {
+
+            Queue<Integer> temp = new LinkedList<>();
+            while (!queue.isEmpty())
+                temp.offer(queue.poll());
+
+            queue.offer(x);
+
+            while (!temp.isEmpty())
+                queue.offer(temp.poll());
+        }
+
+        public int pop() {
+            return queue.poll();
+        }
+
+        public int top() {
+            return queue.peek();
+        }
+
+        public boolean empty() {
+            return queue.isEmpty();
+
+        }
+    }
+
+}
