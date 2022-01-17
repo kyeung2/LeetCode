762e854da784c2f674edac5510ac9b9ef4c51669
Kye Yeung <yeung.kye@pm.me>
2022-01-17 12:15:47 +0000

    Generate parenthesis problem, I could not find a solution to this, the recursion in the solution I found was very nice

---
+++ src/main/java/io/nimbus/leetcode/top100likedquestions_medium/GenerateParenthesis.java
@@ -0,0 +1,63 @@
+package io.nimbus.leetcode.top100likedquestions_medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/generate-parentheses/
+ */
+public class GenerateParenthesis {
+
+    public static void main(String[] args) {
+
+        System.out.println(new GenerateParenthesis().generateParenthesis(3));
+//        System.out.println(new GenerateParenthesis().generateParenthesis_2(3));
+    }
+
+
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<String>();
+        backtrack(list, "", 0, 0, n);
+        return list;
+    }
+
+    public void backtrack(List<String> list, String str, int open, int close, int max) {
+
+        System.out.println("open: "+ open + ", close: "+ close + ", s: "+ str);
+
+        //TODO OK fine i couldn't get the solution so lets learn from this.
+        // TODO key insight 1:the solution is complete WHEN the size of the partial answer is big enough.
+        if (str.length() == max * 2) {
+            System.out.println("adding!");
+            list.add(str);
+            return;
+        }
+
+
+        // it iteratively builds the solution adding to the string as I did. but with the concept of open and closed counters
+        if (open < max)
+            backtrack(list, str + "(", open + 1, close, max);
+
+
+//        // TODO another key insight is that it bleeds the processing into another recursive loop only allowing the close to be called to match up to the open.
+        if (close < open)
+            backtrack(list, str + ")", open, close + 1, max);
+    }
+
+    public List<String> generateParenthesis_2(int n) {
+        List<String> res = new ArrayList<>();
+        helper(res,n,0,"");
+        return res;
+    }
+
+    private void helper(List<String> res, int left, int right, String s) {
+        System.out.println("left: "+ left + ", right: "+ right + ", s: "+ s);
+        if (left == 0 && right == 0) {
+            System.out.println("adding!");
+            res.add(s);
+            return;
+        }
+        if (left > 0) helper(res,left - 1, right + 1, s + "(");
+        if (right > 0) helper(res,left, right - 1, s + ")");
+    }
+}
---
+++ src/main/java/io/nimbus/leetcode/top100likedquestions_medium/GenerateParenthesis_my_attempt.java
@@ -0,0 +1,48 @@
+package io.nimbus.leetcode.top100likedquestions_medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/generate-parentheses/
+ */
+public class GenerateParenthesis_my_attempt {
+
+    public static void main(String[] args) {
+
+
+        // 2
+        // ["(())", "(())()()"]
+        // ["(())", "()()"]
+
+        // 3
+        // ["((()))", "((()))   (())()", "((()))(()) ()(())", "((()))(())()(())()()"]
+        // ["((()))","(()())","(())()","()(())","()()()"]
+        System.out.println(new GenerateParenthesis_my_attempt().generateParenthesis(3));
+    }
+
+    public List<String> generateParenthesis(int n) {
+        List<String> combinations = new ArrayList<>();
+        generateParenthesis_recursive(combinations,  n);
+        return combinations;
+
+    }
+
+    private void generateParenthesis_recursive(List<String> combinations, int n) {
+
+        if(n == 0)
+            return;
+
+
+        // in the second iteration where n is now n-1, we need to add the split
+        var combination = "(".repeat(n ) + ")".repeat(n);
+        combinations.add(combination);
+        generateParenthesis_recursive(combinations, n - 1);
+
+    }
+
+
+
+
+
+}
